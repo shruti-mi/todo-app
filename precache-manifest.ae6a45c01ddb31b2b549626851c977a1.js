4da7cb29e1355f698055a004e4970bc89973867b
shrutimittal1100 <shrutimittal1100@gmail.com>
2020-05-18 19:44:47 +0530

    Updates

---
+++ precache-manifest.ae6a45c01ddb31b2b549626851c977a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf098fbed90571aebb5fed1485ffbd0b",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "7a0ab53c94293aedf73d",
+    "url": "/todo-app/static/css/main.1a25edc8.chunk.css"
+  },
+  {
+    "revision": "d338bbcdf4d28909a4dc",
+    "url": "/todo-app/static/js/2.ed67f86e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.ed67f86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0ab53c94293aedf73d",
+    "url": "/todo-app/static/js/main.547c1991.chunk.js"
+  },
+  {
+    "revision": "672d1f4664f6508537da",
+    "url": "/todo-app/static/js/runtime-main.6751aa9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
